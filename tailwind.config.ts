d3e1d205cc563573abed41a097104dd7d10e072e
Max Burov <gillid@mail.ru>
2024-12-17 14:33:41 +0100

    Remove custom font and color variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,13 +4,5 @@ export default {
   mode: 'jit',
   purge: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
-  theme: {
-    extend: {
-      colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
-      },
-    },
-  },
   plugins: [require('daisyui')],
 } satisfies Config;
