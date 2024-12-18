d037860d9c376d351a9f18f6a4e79b5fc0ef8385
Max Burov <max.gillid@gmail.com>
2024-12-18 12:54:46 +0100

    Add pseudo-auth with iron-session and random token generation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,35 @@
-export default function Home() {
-  return null;
+import { createAuth, deleteAuth, getAuth } from '@/auth';
+
+const Public = () => (
+  <>
+    <h2>Public</h2>
+    <p>
+      <button className='btn' onClick={createAuth}>
+        Authorize
+      </button>
+    </p>
+  </>
+);
+
+const Private = async () => (
+  <>
+    <h2>Private</h2>
+    <p>Token: {(await getAuth())?.uuid}</p>
+    <p>
+      <button className='btn' onClick={deleteAuth}>
+        Delete auth
+      </button>
+    </p>
+  </>
+);
+
+export default async function Home() {
+  const auth = await getAuth();
+
+  return (
+    <div>
+      <h1>Home</h1>
+      {auth ? <Private /> : <Public />}
+    </div>
+  );
 }
