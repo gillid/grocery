c2ba7e2b5a26f099910da4db583c96717cf1d3a9
Max Burov <max.gillid@gmail.com>
2024-12-22 12:58:48 +0100

    Manifest and icons

--- src/_layout/RootLayout.tsx
+++ src/_layout/RootLayout.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Header } from './Header';
 import './globals.css';
+import Head from 'next/head';
 
 export const RootLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
     <html lang='en' data-theme='cupcake'>
+      <Head>
+        <meta name='apple-mobile-web-app-title' content='Grocery' />
+      </Head>
       <body>
         <div className='h-screen overflow-y-scroll overflow-x-hidden relative'>
           <Header />
---
+++ src/app/manifest.ts
@@ -0,0 +1,27 @@
+import type { MetadataRoute } from 'next';
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'Grocery',
+    short_name: 'Grocery',
+    description: 'An app for creating synced grocery list',
+    start_url: '/',
+    display: 'standalone',
+    theme_color: '#DBCA9A',
+    background_color: '#DBCA9A',
+    icons: [
+      {
+        src: '/web-app-manifest-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+      {
+        src: '/web-app-manifest-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+    ],
+  };
+}
